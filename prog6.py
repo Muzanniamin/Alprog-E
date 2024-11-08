5192fadc6cf862714f0eadae1942cd7985d29e60
Muzanniamin <muzaniamin@gmail.com>
2024-11-08 09:48:17 +0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,25 @@
+#variable assigment value#
+x = 9
+y = 6
+print("nilai dari x adalah : ",y)
+
+# aturan penamaan variable
+luas = 50#
+oli_sykes = 125#
+OliX= 18
+oliX = 7
+aldebaran55 = 20
+print("nilai dari Oli adalah:,", aldebaran55)
+
+
+a = 4
+print("tipe data dari a:", type  (a))
+
+b = 6.9
+print("tipe data dari b:", type  (b))
+
+c = "gacor"
+print("tipe data dari c:", type  (c))
+
+d = False
+print("tipe data dari d:", type  (d))
